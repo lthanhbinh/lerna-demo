e6532d1552f649a963c842b49093aec93d33df42
lthanhbinh <lthanhbinh@tma.com.vn>
2023-10-23 17:37:02 +0700

    update my-react-app-to my-app

--- packages/products/src/Products.tsx
+++ packages/products/src/Products.tsx
@@ -1,27 +1,76 @@
 import React from 'react';
 interface ProductsProps {
-    productsCollection: Array<any>,
-    addToCart?(product: object): void;
+  addToCart?(product: object): void;
 }
 
-export function Products({
-    productsCollection,
-    addToCart
-}: ProductsProps) {
-    return (
-      <div>
-          <h1>Products</h1>
-          <ul className={'products'}>
-              {
-                  productsCollection.map((product: any, key: any) => {
-                      return <li key={key}>
-                          <h4>{product.name}</h4>
-                          <p>{product.description}</p>
-                          <button className={'add-to-cart'} onClick={() => addToCart(product)}>Add to cart</button>
-                      </li>
-                  })
-              }
-          </ul>
-      </div>
-    );
+const products = [
+  {
+    id: 1,
+    name: 'Elegance Hair Gel',
+    description:
+      'Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.',
+    price: 9.99,
+    stock: 10567
+  },
+  {
+    id: 2,
+    name: 'Elegance Hair Serum',
+    description:
+      'Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.',
+    price: 14.99,
+    stock: 9956
+  },
+  {
+    id: 3,
+    name: 'Elegance Hair Shampoo',
+    description:
+      'Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.',
+    price: 7.99,
+    stock: 5689
+  },
+  {
+    id: 4,
+    name: 'Elegance Hair Shampoo',
+    description:
+      'Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper lobortis nisl ut ex ea commodo consequat.',
+    price: 10.99,
+    stock: 5689
+  },
+  {
+    id: 5,
+    name: 'Elegance Hair Shampoo 2',
+    description:
+      'Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper lobortis nisl ut ex ea commodo consequat.',
+    price: 10.99,
+    stock: 5689
+  },
+  {
+    id: 6,
+    name: 'Elegance Hair Shampoo 2',
+    description:
+      'Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper lobortis nisl ut ex ea commodo consequat.',
+    price: 10.99,
+    stock: 5689
+  }
+];
+
+export function Products({ addToCart }: ProductsProps) {
+  return (
+    <div>
+      <h1>Products</h1>
+      <ul className={'products'}>
+        {products.map((product: any, key: any) => {
+          return (
+            <li key={key}>
+              <h4>{product.name}</h4>
+              <p>{product.description}</p>
+              <button className={'add-to-cart'} onClick={() => addToCart(product)}>
+                Add to cart
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
