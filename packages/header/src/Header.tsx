a5cece87422c0d632986967392f71c8a52ce49de
lthanhbinh <lthanhbinh@tma.com.vn>
2023-10-24 16:33:10 +0700

    update routing

--- packages/header/src/Header.tsx
+++ packages/header/src/Header.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
+import { NavLink, BrowserRouter as Router } from 'react-router-dom';
 
 interface HeaderProps {
-  counterCart: number,
+  counterCart: number;
+  routes: { pathname: string; name: string; element?: React.ReactNode }[];
 }
 
-export function Header( {
-  counterCart = 0
-}: HeaderProps) {
-    return (
-        <header>
-            <div className={'left-header'}>
-              <ul>
-                <li><a href={'/app'}>App</a></li>
-                <li><a href={'/dashboard'}>Dashboard</a></li>
-                <li><a href={'/products'}>Products</a></li>
-              </ul>
-            </div>
-            <div className={'header-right'}>
-              <div className={'cart-block'}>Cart ({counterCart})</div>
-              <div className={'log-out'}>Log Out</div>
-            </div>
-        </header>
-    );
+export function Header({ counterCart = 0, routes = [] }: HeaderProps) {
+  return (
+    <header>
+      <div
+        className={'left-header'}
+        style={{ display: 'flex', alignItems: 'center', height: '54px' }}>
+        {routes.length > 0 &&
+          routes.map((item, index) => {
+            return (
+              <div className="submenu" style={{ margin: '0px 20px' }} key={index}>
+                <NavLink to={{ pathname: item.pathname }}>{item.name}</NavLink>
+              </div>
+            );
+          })}
+      </div>
+      <div className={'header-right'}>
+        <div className={'cart-block'}>Cart ({counterCart})</div>
+        <div className={'log-out'}>Log Out</div>
+      </div>
+    </header>
+  );
 }
--- packages/my-app/src/routes/AppRouters.tsx
+++ packages/my-app/src/routes/AppRouters.tsx
@@ -10,31 +10,40 @@ const AppRouter = () => {
     setCartProduct(counterProduct + 1);
   };
 
+  const menu = [
+    {
+      pathname: '/app',
+      name: 'Application',
+      element: (
+        <div style={{ height: 'calc(100vh - 200px)', textAlign: 'center', margin: '50px' }}>
+          Dashboard
+        </div>
+      )
+    },
+    {
+      pathname: '/dashboard',
+      name: 'Dashboard',
+      element: (
+        <div style={{ height: 'calc(100vh - 200px)', textAlign: 'center', margin: '50px' }}>
+          Dashboard
+        </div>
+      )
+    },
+    {
+      pathname: '/products',
+      name: 'Products',
+      element: <Products addToCart={addProductToCart} />
+    }
+  ];
+
   return (
     <div className="App">
-      <Header counterCart={counterProduct} />
       <Router>
+        <Header counterCart={counterProduct} routes={menu} />
         <Routes>
-          <Route
-            path="/products"
-            element={<Products addToCart={addProductToCart} />}
-          />
-          <Route
-            path="/dashboard"
-            element={
-              <div style={{ height: 'calc(100vh - 200px)', textAlign: 'center', margin: '50px' }}>
-                Dashboard
-              </div>
-            }
-          />
-          <Route
-            path="/app"
-            element={
-              <div style={{ height: 'calc(100vh - 200px)', textAlign: 'center', margin: '50px' }}>
-                Application
-              </div>
-            }
-          />
+          {menu.map((item) => (
+            <Route path={item.pathname} element={item.element} />
+          ))}
         </Routes>
       </Router>
       <Footer />
@@ -43,5 +52,3 @@ const AppRouter = () => {
 };
 
 export default AppRouter;
-
-
